15c972f0d181cef62ef8b2087603c5675e9fbc1a
mythlesh <mythlesh114@gmail.com>
2025-11-26 14:34:16 +0545

    Fix case sensitivity

---
+++ src/data/servicesData.js
@@ -0,0 +1,27 @@
+const servicesData = [
+  {
+    id: 1,
+    title: "Job Visa",
+    image: "/images/service-1.jpg",
+    delay: "1s",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Mollitia fugit dolores nesciunt adipisci obcaecati veritatis cum.",
+  },
+  {
+    id: 2,
+    title: "Business Visa",
+    image: "/images/service-2.jpg",
+    delay: "3s",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Mollitia fugit dolores nesciunt adipisci obcaecati veritatis cum.",
+  },
+  {
+    id: 3,
+    title: "Diplomatic Visa",
+    image: "/images/service-3.jpg",
+    delay: "5s",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Mollitia fugit dolores nesciunt adipisci obcaecati veritatis cum.",
+  },
+];
+export default servicesData;
